4b19af65b602360e47055ab662954bfa5e095707
AEthelheard <htang798@usc.edu>
2021-04-02 20:01:58 -0700

    done section 10

--- Destini-iOS13/Controller/ViewController.swift
+++ Destini-iOS13/Controller/ViewController.swift
@@ -13,12 +13,25 @@ class ViewController: UIViewController {
     @IBOutlet weak var storyLabel: UILabel!
     @IBOutlet weak var choice1Button: UIButton!
     @IBOutlet weak var choice2Button: UIButton!
+    var storyBrain = StoryBrain()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        updateUI()
     }
-
+    
+    
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        let userAnswer = sender.currentTitle!
+        storyBrain.choicePath(userAnswer)
+        Timer.scheduledTimer(timeInterval: 0.5, target: self,  selector: #selector(updateUI), userInfo:nil, repeats: false)
+    }
+    
+    @objc func updateUI() {
+        storyLabel.text = storyBrain.getTitle()
+        choice1Button.setTitle(storyBrain.getChoiceOne(), for: .normal)
+        choice2Button.setTitle(storyBrain.getChoiceTwo(), for: .normal)
+    }
 
 }
 
--- Destini-iOS13/Model/Story.swift
+++ Destini-iOS13/Model/Story.swift
@@ -7,3 +7,25 @@
 //
 
 import Foundation
+//
+//title: "Your car has blown a tire on a winding road in the middle of nowhere with no cell phone reception. You decide to hitchhike. A rusty pickup truck rumbles to a stop next to you. A man with a wide brimmed hat with soulless eyes opens the passenger door for you and asks: 'Need a ride, boy?'.",
+//choice1: "I'll hop in. Thanks for the help!", choice1Destination: 2,
+//choice2: "Better ask him if he's a murderer first.", choice2Destination: 1
+
+struct Story {
+    let title: String
+    let choice1: String
+    let choice2: String
+    let choice1Destination: Int
+    let choice2Destination: Int
+    
+    init(title: String, choice1: String, choice1Destination: Int, choice2: String, choice2Destination: Int) {
+        self.title = title
+        self.choice1 = choice1
+        self.choice2 = choice2
+        self.choice1Destination = choice1Destination
+        self.choice2Destination = choice2Destination
+    }
+    
+}
+
